ab79ec8cb6a868b9988c5fa726b6f5be451b3c2a
5tr1ker <tjseocld@naver.com>
2023-07-13 17:42:01 +0900

    init: 프로젝트 초기 설정
    프로젝트 초기 설정

---
+++ src/main/java/com/team/heyyo/HeyyoApplication.java
@@ -0,0 +1,11 @@
+package com.team.heyyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyyoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HeyyoApplication.class, args);
+    }
+}
