7f21958898f07f1b7955df57ee1e72d22aef6091
wan2daaa <wan2daaa@gmail.com>
2023-07-21 01:00:39 +0900

    feat: jwt 관련 issuer, secret_key 를 클래스로 사용하게 구현

---
+++ src/main/java/com/team/heyyo/config/jwt/constant/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.team.heyyo.config.jwt.constant;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
