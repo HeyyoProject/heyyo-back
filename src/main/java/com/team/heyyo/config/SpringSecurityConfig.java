12b186ff20b0ea6060631ef5b42e3b0cc13eff6c
wan2daaa <wan2daaa@gmail.com>
2023-07-21 01:08:17 +0900

    feat: 스프링 시큐리티 Config 작성
    csrf, cors, httpBasic, form, session
    다 사용하지 않아서 막아 두었습니다
    
    filterChain 이후는
    
    사용할 시큐리티 관련 구현체에 DI해주는 코드 입니다

--- src/main/java/com/team/heyyo/config/SpringSecurityConfig.java
+++ src/main/java/com/team/heyyo/config/SpringSecurityConfig.java
@@ -1,44 +1,93 @@
 package com.team.heyyo.config;
 
+import com.team.heyyo.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import com.team.heyyo.config.oauth.OAuth2SuccessHandler;
+import com.team.heyyo.config.oauth.Oauth2UserCustomService;
+import com.team.heyyo.user.service.UserService;
+import com.team.heyyo.config.jwt.repository.RefreshTokenRepository;
+import com.team.heyyo.config.jwt.support.TokenAuthenticationFilter;
+import com.team.heyyo.config.jwt.support.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig {
+    private final Oauth2UserCustomService oauth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
 
-    /**
-     * TODO. JWT, OAUTH2 구현 필요
-     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .cors().disable()
             .httpBasic().disable()
-            // 세션을 사용하지 않기 때문에 STATELESS로 설정
+            .formLogin().disable()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             // H2 콘솔 사용을 위한 설정
             .and().headers(headers -> headers.frameOptions().sameOrigin());
 
-        http
-            .authorizeHttpRequests()
-            .requestMatchers("/login/**").permitAll()
-            .requestMatchers("/signup/**").permitAll()
-            .requestMatchers(PathRequest.toH2Console()).permitAll()// h2-console
-            .anyRequest().authenticated(); // 그 외 인증 없이 접근X
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+//      토큰 재발급 URL 은 열어두고, 나머지 API 는 인증 필요
+        http.authorizeHttpRequests()
+                .requestMatchers(toH2Console()).permitAll()
+                .requestMatchers("/api/token").permitAll()
+                .anyRequest().authenticated();
+
+        http.oauth2Login()
+                .authorizationEndpoint()
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+                .and()
+                .successHandler(oauth2SuccessHandler())
+                .userInfoEndpoint()
+                .userService(oauth2UserCustomService);
+
+//        로그아웃은 클라이언트 쪽에서 쿠키와 header 지우면 되서 따로 구현 안했습니다
+
+        http.exceptionHandling()
+                .defaultAuthenticationEntryPointFor(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                        new AntPathRequestMatcher("/api/**")
+                );
 
         return http.build();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oauth2SuccessHandler() {
+        return new OAuth2SuccessHandler(
+                tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService
+        );
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
     }
 
     /* 비밀번호 암호화 */
