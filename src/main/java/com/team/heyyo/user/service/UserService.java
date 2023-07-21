5e74c4277dd3b93448eb2f8c99c2f5d779a55d78
wan2daaa <wan2daaa@gmail.com>
2023-07-21 23:50:54 +0900

    refact: BCryptPasswordEncoder PasswordEncoder 인터페이스로 들고오게 설정
    @Configuration 파일에 DI 를 해줬는데 실수로 구현체를 주입 해서 수정하였습니다

--- src/main/java/com/team/heyyo/user/service/UserService.java
+++ src/main/java/com/team/heyyo/user/service/UserService.java
@@ -3,7 +3,7 @@ package com.team.heyyo.user.service;
 import com.team.heyyo.user.domain.User;
 import com.team.heyyo.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,19 +11,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * FIXME : 회원가입시 비밀번호 암호화 하는 방법 간단하게 구현 해놓았습니다.
      * @return
      */
     public Long save(){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         String password = "test";
 
         return userRepository.save(
                 User.builder()
-                        .password(encoder.encode(password))
+                        .password(passwordEncoder.encode(password))
                         .build()
                 )
                 .getId();
