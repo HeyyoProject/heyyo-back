524e8c7c45a575c434a5658b50d594432c9e49c3
wan2daaa <wan2daaa@gmail.com>
2023-07-22 23:20:07 +0900

    refact: TokenAuthenticationFilter accessToken 재발급 로직 추가
    accessToken을 검증하고,
    -> 검증에 실패하면  refreshToken를 검증한다.
    검증에 통과하면,
    -> accessToken을 header에 넣어준다

--- src/main/java/com/team/heyyo/auth/jwt/support/TokenAuthenticationFilter.java
+++ src/main/java/com/team/heyyo/auth/jwt/support/TokenAuthenticationFilter.java
@@ -1,7 +1,11 @@
 package com.team.heyyo.auth.jwt.support;
 
+import com.team.heyyo.auth.jwt.service.RefreshTokenService;
+import com.team.heyyo.user.domain.User;
+import com.team.heyyo.user.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,28 +15,64 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static com.team.heyyo.auth.jwt.constant.JwtTokenDuration.ACCESS_TOKEN_EXPIRED;
+
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+    
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     private static final String HEADER_AUTHORIZATION = "Authorization";
     private static final String TOKEN_PREFIX = "Bearer ";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-//      accessToken을 요청헤더의 Authorization 키의 값으로 조회 한다.
-        String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+        String token = extractAccessToken(request);
 
-//      위의 값에서 접두사인 'Bearer '을 제거한다
-        String token = getAccessToken(authorizationHeader);
-
-//      가져온 토큰이 유효한지 검사하고, 유효하면 -> SecurityContextHolder 에 인증 정보를 설정해준다
         if (tokenProvider.validToken(token)) {
-            Authentication authentication = tokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
+            setAuthenticationInSecurityContextHolder(token);
+        } else {
+            Cookie[] cookies = request.getCookies();
+            for (Cookie cookie : cookies) {
+                if (REFRESH_TOKEN_COOKIE_NAME.equals(cookie.getName())) {
+                    String refreshToken = cookie.getValue();
 
+                    if (tokenProvider.validToken(refreshToken)) {
+                        String accessToken = generateNewAccessToken(response, refreshToken);
+                        setAuthenticationInSecurityContextHolder(accessToken);
+                    }
+                    break;
+                }
+            }
+        } 
         filterChain.doFilter(request, response);
+    }
+
+    private String extractAccessToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+        String token = getAccessToken(authorizationHeader);
+        return token;
+    }
+
+    private void setAuthenticationInSecurityContextHolder(String accessToken) {
+        Authentication authentication = tokenProvider.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String generateNewAccessToken(HttpServletResponse response, String refreshToken) {
+        User user = findUserWithRefreshToken(refreshToken);
+        String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_EXPIRED.getDuration());
+        response.setHeader(HEADER_AUTHORIZATION, TOKEN_PREFIX + accessToken);
+        return accessToken;
+    }
+
+    private User findUserWithRefreshToken(String refreshToken) {
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+        return user;
     }
 
     private String getAccessToken(String authorizationHeader) {
