388e703561f8b40cd45b39b665e63cc45235e6f7
wan2daaa <wan2daaa@gmail.com>
2023-08-04 23:13:02 +0900

    refeat: enum의 name 의미가 불분명하여 수정

--- src/main/java/com/team/heyyo/user/constant/UserResponseCode.java
+++ src/main/java/com/team/heyyo/user/constant/UserResponseCode.java
@@ -6,13 +6,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserResponseCode {
-    SUCCESS(200, "성공"),
-    ID_NOT_FOUND(400, "id와 일치하는 user가 없습니다."),
-    PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다."),
-    EMAIL_NOT_FOUND(400, "해당 email과 일치하는 user가 없습니다."),
-    EMAIL_DUPLICATION(400, "이미 존재하는 email입니다."),
-    NAME_DUPLICATION(400, "이미 존재하는 name입니다.");
+  SUCCESS(200, "성공"),
+  ID_NOT_FOUND(404, "id와 일치하는 회원이 없습니다."),
+  PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다."),
+  EMAIL_NOT_FOUND(404, "해당 이메일과 일치하는 회원이 없습니다."),
+  EMAIL_DUPLICATION(400, "이미 존재하는 이메일입니다."),
+  NAME_PASSWORD_NOT_MATCH(404, "이름과 비밀번호와 일치하는 회원이 없습니다.");
 
-    private final int status;
-    private final String message;
+  private final int status;
+  private final String message;
 }
