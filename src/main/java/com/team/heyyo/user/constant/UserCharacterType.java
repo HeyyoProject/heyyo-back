0acf9c6c75429d807d347bbf9a16a6710b1aa91d
wan2daaa <wan2daaa@gmail.com>
2023-08-12 22:54:01 +0900

    feat: RequestDto 와 캐릭터 관련 enum 정의

---
+++ src/main/java/com/team/heyyo/user/constant/UserCharacterType.java
@@ -0,0 +1,15 @@
+package com.team.heyyo.user.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+public enum UserCharacterType {
+  고독,
+  북적,
+  소심,
+  소통,
+  영구,
+  유익,
+  집중,
+  한산;
+}
