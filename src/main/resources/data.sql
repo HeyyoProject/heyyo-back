99f84bfeaa2b9695d21ea500f1053c7967a22148
wan2daaa <wan2daaa@gmail.com>
2023-08-27 23:52:46 +0900

    feat: 임시 데이터 생성 및 data.sql 적용위해 yaml 파일에 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,83 @@
+insert into user_tb (email, password, nickname, name)
+values ('test@naver.com', '$2y$10$A.GI9LepwCc9hLaNQX8xnOOULfUH.so1tZ3L7fbEg701GsBfPFTsi', '테스트닉네임', '테스트이름');
+
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test1', 'description1', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test2', 'description2', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test3', 'description3', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test4', 'description4', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test5', 'description5', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test6', 'description6', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test7', 'description7', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test8', 'description8', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test9', 'description9', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test10', 'description10', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test11', 'description11', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test12', 'description12', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test13', 'description13', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test14', 'description14', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test15', 'description15', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test16', 'description16', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test17', 'description17', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test18', 'description18', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test19', 'description19', 'session', now());
+insert into group_study_tb (owner_user_id, title, description, session, created_at)
+values (1, 'test20', 'description20', 'session', now());
+
+insert into group_study_like_tb (group_study_id, created_at)
+values (1, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (1, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (1, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (1,now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (1, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (2, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (2, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (2, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (2, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (3, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (3, now());
+insert into group_study_like_tb (group_study_id, created_at)
+values (3, now());
+
+insert into group_study_tag_tb (group_study_id, tag_data)
+values (1, 'tag_data1');
+insert into group_study_tag_tb (group_study_id, tag_data)
+values (1, 'tag_data2');
+insert into group_study_tag_tb (group_study_id, tag_data)
+values (1, 'tag_data3');
+
+insert into group_study_tag_tb (group_study_id, tag_data)
+values (2, 'tag_data4');
+insert into group_study_tag_tb (group_study_id, tag_data)
+values (2, 'tag_data5');
+
+insert into group_study_tag_tb (group_study_id, tag_data)
+values (3, 'tag_data6');
