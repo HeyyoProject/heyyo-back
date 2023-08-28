be22b81a88183d3455de807362c5ee1227f7d8d9
wan2daaa <wan2daaa@gmail.com>
2023-08-28 11:43:07 +0900

    feat: stream으로는 복잡도가 올라가서 쿼리에서 분류하는 방식으로 구현

--- src/main/java/com/team/heyyo/group/study/domain/GroupStudyTag.java
+++ src/main/java/com/team/heyyo/group/study/domain/GroupStudyTag.java
@@ -2,6 +2,7 @@ package com.team.heyyo.group.study.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,4 +18,10 @@ public class GroupStudyTag {
     private Long groupStudyId;
 
     private String tagData;
+
+    @Builder
+    private GroupStudyTag(Long groupStudyId, String tagData) {
+        this.groupStudyId = groupStudyId;
+        this.tagData = tagData;
+    }
 }
