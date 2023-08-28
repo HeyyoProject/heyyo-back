f63596ef3e6df744ffec5307f082769d4a91d7fa
wan2daaa <wan2daaa@gmail.com>
2023-08-29 03:36:07 +0900

    rename: 클래스명이 포괄적이라서 조금더 디테일하게 클래스명 수정

--- src/main/java/com/team/heyyo/group/study/service/GroupStudyMainPageListService.java
+++ src/main/java/com/team/heyyo/group/study/service/GroupStudyMainPageListService.java
@@ -2,10 +2,9 @@ package com.team.heyyo.group.study.service;
 
 import com.team.heyyo.auth.jwt.support.TokenProvider;
 import com.team.heyyo.group.study.domain.GroupStudy;
-import com.team.heyyo.group.study.domain.GroupStudyTag;
-import com.team.heyyo.group.study.dto.GroupStudyResponse;
-import com.team.heyyo.group.study.repository.groupstudylike.GroupStudyLikeRepository;
+import com.team.heyyo.group.study.dto.GroupStudyListResponse;
 import com.team.heyyo.group.study.repository.groupstudy.GroupStudyRepository;
+import com.team.heyyo.group.study.repository.groupstudylike.GroupStudyLikeRepository;
 import com.team.heyyo.group.study.repository.groupstudytag.GroupStudyTagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,49 +15,52 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @RequiredArgsConstructor
 @Service
-public class GroupStudyService {
+public class GroupStudyMainPageListService {
 
     private final GroupStudyRepository groupStudyRepository;
     private final GroupStudyTagRepository groupStudyTagRepository;
     private final GroupStudyLikeRepository groupStudyLikeRepository;
     private final TokenProvider tokenProvider;
 
-    public List<GroupStudyResponse> getRecentGroupStudyList(String accessToken) {
-
-//        FIXME : 회원이 세션에 몇명 로그인 됐는지 바뀔수 있기 때문에 일단 임시로 랜덤값 삽입
-        int randomNumber = ThreadLocalRandom.current().nextInt(100, 1000);
+    public List<GroupStudyListResponse> getRecentGroupStudyList(String accessToken) {
+        int randomNumber = getRandomNumber();
         Long userId = tokenProvider.getUserId(accessToken);
 
-        List<GroupStudyResponse> groupStudyResponses = new ArrayList<>();
+        List<GroupStudyListResponse> groupStudyListResponseList = new ArrayList<>();
 
         List<GroupStudy> groupStudies = groupStudyRepository.selectRecentGroupStudies();
         for (GroupStudy groupStudy : groupStudies) {
             boolean isUserLikedThisGroupStudy = isUserLikedThisGroupStudy(userId, groupStudy);
 
             List<String> groupStudyTagList = getGroupStudyTagList(groupStudy);
-            groupStudyResponses
-                    .add(GroupStudyResponse.of(groupStudy.getTitle(), groupStudyTagList, randomNumber, isUserLikedThisGroupStudy));
+            groupStudyListResponseList
+                    .add(GroupStudyListResponse.of(groupStudy.getTitle(), groupStudyTagList, randomNumber, isUserLikedThisGroupStudy));
         }
-        return groupStudyResponses;
+        return groupStudyListResponseList;
     }
 
-    public List<GroupStudyResponse> getBestGroupStudyListFromToday(String accessToken) {
-
-        //        FIXME : 회원이 세션에 몇명 로그인 됐는지 바뀔수 있기 때문에 일단 임시로 랜덤값 삽입
-        int randomNumber = ThreadLocalRandom.current().nextInt(100, 1000);
+    public List<GroupStudyListResponse> getBestGroupStudyListFromToday(String accessToken) {
+        int randomNumber = getRandomNumber();
         Long userId = tokenProvider.getUserId(accessToken);
-        List<GroupStudyResponse> groupStudyResponses = new ArrayList<>();
+        List<GroupStudyListResponse> groupStudyListResponseList = new ArrayList<>();
 
         List<GroupStudy> groupStudiesOrderedByMostLikesFromToday = groupStudyRepository.findGroupStudiesOrderedByMostLikesFromToday();
         for (GroupStudy groupStudy : groupStudiesOrderedByMostLikesFromToday) {
             boolean isUserLikedThisGroupStudy = isUserLikedThisGroupStudy(userId, groupStudy);
 
             List<String> groupStudyTagList = getGroupStudyTagList(groupStudy);
-            groupStudyResponses
-                    .add(GroupStudyResponse.of(groupStudy.getTitle(), groupStudyTagList, randomNumber, isUserLikedThisGroupStudy));
+            groupStudyListResponseList
+                    .add(GroupStudyListResponse.of(groupStudy.getTitle(), groupStudyTagList, randomNumber, isUserLikedThisGroupStudy));
         }
-        return groupStudyResponses;
+        return groupStudyListResponseList;
 
+    }
+
+
+
+//        FIXME : 회원이 세션에 몇명 로그인 됐는지 바뀔수 있기 때문에 일단 임시로 랜덤값 삽입
+    private int getRandomNumber() {
+        return ThreadLocalRandom.current().nextInt(100, 1000);
     }
 
 
