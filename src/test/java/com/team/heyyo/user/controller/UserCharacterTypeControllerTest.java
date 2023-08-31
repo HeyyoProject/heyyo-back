519c7fce6ac0f34c39a6d683acdddbe23486515d
wan2daaa <wan2daaa@gmail.com>
2023-09-01 04:06:13 +0900

    refactor: WebMvcTest에서 기본으로 사용하는 Controller 테스트 어노테이션으로 수정

--- src/test/java/com/team/heyyo/user/controller/UserCharacterTypeControllerTest.java
+++ src/test/java/com/team/heyyo/user/controller/UserCharacterTypeControllerTest.java
@@ -1,48 +1,65 @@
 package com.team.heyyo.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team.heyyo.user.constant.UserCharacterType;
+import com.team.heyyo.user.dto.UserBaseResponse;
+import com.team.heyyo.user.dto.UserTypeRequest;
+import com.team.heyyo.user.service.UserCharacterTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.headers.HeaderDocumentation;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.team.heyyo.user.constant.UserCharacterType;
-import com.team.heyyo.user.dto.UserBaseResponse;
-import com.team.heyyo.user.dto.UserTypeRequest;
-import com.team.heyyo.user.service.UserCharacterTypeService;
-import java.nio.charset.StandardCharsets;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.headers.HeaderDocumentation;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-
+@SpringBootTest
 @AutoConfigureRestDocs
-@WebMvcTest(controllers = UserCharacterTypeController.class)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@MockBean(JpaMetamodelMappingContext.class)
+@WebAppConfiguration
 class UserCharacterTypeControllerTest {
 
   @Autowired
   ObjectMapper objectMapper;
 
-  @Autowired
   MockMvc mockMvc;
+
+    @BeforeEach
+    public void init(WebApplicationContext webApplicationContext,
+                     RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
 
   @MockBean
   UserCharacterTypeService userCharacterTypeService;
