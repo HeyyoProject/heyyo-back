d042771225a2f9f9be627f7b0c9fd7c69e8694a0
5tr1ker <tjseocld@naver.com>
2023-09-16 16:27:46 +0900

    fix: 소켓 핸드러 타입변경
    fix: 소켓 핸드러 타입변경

--- src/main/java/com/team/heyyo/group/chat/config/HandlerChatConfig.java
+++ src/main/java/com/team/heyyo/group/chat/config/HandlerChatConfig.java
@@ -2,7 +2,7 @@ package com.team.heyyo.group.chat.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
